2e9bf5f76f4d0e25e5cca35e7f804a2495937e82
Meeqat Suharwardy <meeqatsuharwardy@Meeqats-MacBook-Pro.local>
2023-06-11 19:38:20 +0100

    main

--- final/settings.py
+++ final/settings.py
@@ -129,7 +129,6 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-django_heroku.settings(locals())
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
